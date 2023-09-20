b0e089ea845433b01d928acbd17b1a0441660201
tolu13 <tolulopeolanrewaju8@gmail.com>
2023-09-19 18:08:22 -0700

    simpleshell

---
+++ strings.c
@@ -0,0 +1,151 @@
+#include "my_shell.h"
+
+/**
+ * custom_str_len - returns the length of a string.
+ * @string: pointer to string.
+ * Return: length of string.
+ */
+int custom_str_len(char *string)
+{
+	int length = 0;
+
+	if (string == NULL)
+		return (0);
+
+	while (string[length++] != '\0')
+	{
+	}
+	return (--length);
+}
+
+/**
+ * custom_str_duplicate - duplicates an string
+ * @string: String to be copied
+ * Return: pointer to the array
+ */
+char *custom_str_duplicate(char *string)
+{
+	char *result;
+	int length, i;
+
+	if (string == NULL)
+		return (NULL);
+
+	length = custom_str_len(string) + 1;
+
+	result = malloc(sizeof(char) * length);
+
+	if (result == NULL)
+	{
+		perror("Error");
+		return (NULL);
+	}
+	for (i = 0; i < length ; i++)
+	{
+		result[i] = string[i];
+	}
+
+	return (result);
+}
+
+/**
+ * custom_str_compare - Compare two strings
+ * @string1: String one, or the shorter
+ * @string2: String two, or the longer
+ * @number: number of characters to be compared, 0 if infinite
+ * Return: 1 if the strings are equals, 0 if the strings are different
+ */
+int custom_str_compare(char *string1, char *string2, int number)
+{
+	int iterator;
+
+	if (string1 == NULL && string2 == NULL)
+		return (1);
+
+	if (string1 == NULL || string2 == NULL)
+		return (0);
+
+	if (number == 0) /* Infinite length */
+	{
+		if (custom_str_len(string1) != custom_str_len(string2))
+			return (0);
+		for (iterator = 0; string1[iterator]; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+				return (0);
+		}
+		return (1);
+	}
+	else /* If there is a number of chars to be compared */
+	{
+		for (iterator = 0; iterator < number ; iterator++)
+		{
+			if (string1[iterator] != string2[iterator])
+			return (0);
+		}
+		return (1);
+	}
+}
+
+/**
+ * str_concat - concatenates two strings.
+ * @string1: String to be concatenated
+ * @string2: String to be concatenated
+ *
+ * Return: pointer to the array
+ */
+char *str_concat(char *string1, char *string2)
+{
+	char *result;
+	int length1 = 0, length2 = 0;
+
+	if (string1 == NULL)
+		string1 = "";
+	length1 = custom_str_len(string1);
+
+	if (string2 == NULL)
+		string2 = "";
+	length2 = custom_str_len(string2);
+
+	result = malloc(sizeof(char) * (length1 + length2 + 1));
+	if (result == NULL)
+	{
+		perror("Error");
+		return (NULL);
+	}
+
+	/* Cp str1 */
+	for (length1 = 0; string1[length1] != '\0'; length1++)
+		result[length1] = string1[length1];
+	free(string1);
+
+	/* Cp str2 */
+	for (length2 = 0; string2[length2] != '\0'; length2++)
+	{
+		result[length1] = string2[length2];
+		length1++;
+	}
+
+	result[length1] = '\0';
+	return (result);
+}
+
+/**
+ * custom_str_reverse - this reverses a string.
+ *
+ * @string: pointer to string.
+ * Return: void.
+ */
+void custom_str_reverse(char *string)
+{
+
+	int i = 0, length = custom_str_len(string) - 1;
+	char hold;
+
+	while (i < length)
+	{
+		hold = string[i];
+		string[i++] = string[length];
+		string[length--] = hold;
+	}
+}
